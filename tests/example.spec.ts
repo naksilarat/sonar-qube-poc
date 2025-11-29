4acfb166f79f36e946f0d7ac1a02f7a0566afba3
Ice Arisara <arisara.khl@qackskills.com>
2025-11-29 19:41:11 +0700

    updatev.0.4

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -21,7 +21,7 @@ test.skip("get started link", async ({ page }) => {
 // FAILED TEST
 test("intentional failure test", async () => {
   // This test will always fail to demonstrate failure reporting
-  expect(true).toBe(true);
+  expect(true).toBe(false);
 });
 
 // SKIPPED TEST
